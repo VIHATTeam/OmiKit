0626382c84da4e590fc391cb77ebf33b76466059
HungTH <tranhoaihung01@gmail.com>
2023-10-18 21:43:40 +0700

    <commit> pump version

--- build.sh
+++ build.sh
@@ -1,3 +1,17 @@
 git tag $1
 git push origin $1
-pod trunk push
+pod lib lint --allow-warnings --no-clean --verbose
+
+if [ $? -eq 0 ]; then
+	echo "congratulation,pod verifys success! 💐 💐 💐 💐"
+
+	pod trunk push OmiKit.podspec --allow-warnings --verbose
+
+	if [ $? -eq 0 ]; then
+		echo "congratulation,pod push to midea-specs success! 💐 💐 💐 💐"
+	else
+		echo "pod push to midea-specs fail!"
+	fi
+else
+	echo "pod verifys fail!"
+fi
