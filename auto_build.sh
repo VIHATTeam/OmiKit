ea7225925c95edb8cf698a2498b5efff6a9236fb
HungTH <tranhoaihung01@gmail.com>
2023-11-24 10:57:28 +0700

    <commit> commit file

--- auto_build.sh
+++ auto_build.sh
@@ -3,26 +3,38 @@
 # Đường dẫn đến file podspec
 PODSPEC_FILE="OmiKit.podspec"
 
+# Hàm tăng version
+increment_version() {
+    local version=$1
+    local IFS=.
+    local i ver=($version)
+    for ((i=${#ver[@]}-1; i>=0; i--)); do
+        ((ver[i]++))
+        if [ ${ver[i]} -lt 10 ]; then
+            break
+        fi
+        ver[i]=0
+    done
+    echo "${ver[*]}"
+}
+
 # Lấy version hiện tại từ podspec
-CURRENT_VERSION=$(grep -o 'version = "[0-9]*\.[0-9]*\.[0-9]*"' $PODSPEC_FILE | grep -o '[0-9]*\.[0-9]*\.[0-9]*')
+CURRENT_VERSION=$(grep -o 'version *= *"[0-9]*\.[0-9]*\.[0-9]*"' $PODSPEC_FILE | grep -o '[0-9]*\.[0-9]*\.[0-9]*')
 
-# Tách version thành các thành phần
-IFS='.' read -r -a VERSION_PARTS <<< "$CURRENT_VERSION"
-
-# Tăng patch version (phần tử thứ 3)
-((VERSION_PARTS[2]++))
-
-# Tạo version mới
-NEW_VERSION="${VERSION_PARTS[0]}.${VERSION_PARTS[1]}.${VERSION_PARTS[2]}"
+# Tăng version
+NEW_VERSION=$(increment_version $CURRENT_VERSION)
 
 # Cập nhật file podspec với version mới
 sed -i '' "s/$CURRENT_VERSION/$NEW_VERSION/g" $PODSPEC_FILE
 
-# Commit và push thay đổi
-git add $PODSPEC_FILE
-git commit -m "Increase version to $NEW_VERSION"
-git push origin master
+# echo "Updated podspec to version $NEW_VERSION"
 
-# Tạo và push tag mới
-git tag $NEW_VERSION
-git push origin $NEW_VERSION
+# Commit và push thay đổi
+# git add $PODSPEC_FILE
+# git commit -m "Increase version to $NEW_VERSION"
+# git push origin master
+
+# # Tạo và push tag mới
+# git tag $NEW_VERSION
+# git push origin $NEW_VERSION
+sh push_tag.sh $NEW_VERSION
